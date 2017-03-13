3106c534ea19b12aba95e53f9396295c1abc7f33
peroon <fromjpn@gmail.com>
2017-03-14 03:57:41 +0900

    Save video to not app directory but camera roll

--- swift_video_camera/swift_video_camera/ViewController.swift
+++ swift_video_camera/swift_video_camera/ViewController.swift
@@ -1,67 +1,66 @@
 import UIKit
+import Photos
 import AVFoundation
 
 class ViewController: UIViewController, AVCaptureFileOutputRecordingDelegate {
 
-    private var myVideoOutput: AVCaptureMovieFileOutput!
-    private var myButtonStart: UIButton!
-    private var myButtonStop: UIButton!
+    var myVideoOutput: AVCaptureMovieFileOutput!
+    var myButtonStart: UIButton!
+    var myButtonStop: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // セッションの作成.
-        let mySession = AVCaptureSession()
+        let captureSession = AVCaptureSession()
         
         // デバイス.
         let myDevice = AVCaptureDevice.defaultDevice(withMediaType: AVMediaTypeVideo)
         //let myDevice : AVCaptureDevice!
         
-        // 出力先を生成.
-        let myImageOutput = AVCapturePhotoOutput()
-        
-        // マイクを取得.
-        let audioCaptureDevice = AVCaptureDevice.devices(withMediaType: AVMediaTypeAudio)
-        
-        // マイクをセッションのInputに追加.
-        let audioInput = try! AVCaptureDeviceInput.init(device: audioCaptureDevice?.first as! AVCaptureDevice)
-        
-        // バックライトをmyDeviceに格納.
-//        for device in devices! {
-//            if(device.position == AVCaptureDevicePosition.back){
-//                myDevice = device as? AVCaptureDevice
-//            }
-//        }
- 
-        // バックカメラを取得.
-        let videoInput = try! AVCaptureDeviceInput.init(device: myDevice)
-        
-        // ビデオをセッションのInputに追加.
-        mySession.addInput(videoInput)
-        
-        // オーディオをセッションに追加.
-        mySession.addInput(audioInput)
-        
-        // セッションに追加.
-        mySession.addOutput(myImageOutput)
-        
-        // 動画の保存.
-        myVideoOutput = AVCaptureMovieFileOutput()
-        
-        // ビデオ出力をOutputに追加.
-        mySession.addOutput(myVideoOutput)
-        
-        // 画像を表示するレイヤーを生成.
-        let myVideoLayer = AVCaptureVideoPreviewLayer.init(session: mySession)
-        myVideoLayer?.frame = self.view.bounds
-        myVideoLayer?.videoGravity = AVLayerVideoGravityResizeAspectFill
-        
-        // Viewに追加.
-        self.view.layer.addSublayer(myVideoLayer!)
-        
-        // セッション開始.
-        mySession.startRunning()
-        
+//        let audioCaptureDevices = AVCaptureDevice.devices(withMediaType: AVMediaTypeAudio)
+//        let audioInput = try! AVCaptureDeviceInput.init(device: audioCaptureDevices?.first as! AVCaptureDevice)
+
+        let audioDevice = AVCaptureDevice.defaultDevice(withMediaType: AVMediaTypeAudio)
+        //var audioInput:AVCaptureInput
+        do{
+            let audioInput = try AVCaptureDeviceInput(device: audioDevice) as AVCaptureInput
+            // バックライトをmyDeviceに格納.
+            //        for device in devices! {
+            //            if(device.position == AVCaptureDevicePosition.back){
+            //                myDevice = device as? AVCaptureDevice
+            //            }
+            //        }
+            
+            // バックカメラを取得.
+            let videoInput = try! AVCaptureDeviceInput.init(device: myDevice)
+            
+            //let myImageOutput = AVCapturePhotoOutput()
+            myVideoOutput = AVCaptureMovieFileOutput()
+            
+            captureSession.addInput(videoInput)
+            captureSession.addInput(audioInput)
+            //captureSession.addOutput(myImageOutput)
+            captureSession.addOutput(myVideoOutput)
+            
+            // 画像を表示するレイヤーを生成.
+            let myVideoLayer = AVCaptureVideoPreviewLayer.init(session: captureSession)
+            myVideoLayer?.frame = self.view.bounds
+            myVideoLayer?.videoGravity = AVLayerVideoGravityResizeAspectFill
+            
+            // Viewに追加.
+            self.view.layer.addSublayer(myVideoLayer!)
+            
+            // セッション開始.
+            captureSession.startRunning()
+            
+            self.addButtons()
+
+        }catch{
+            print(error)
+        }
+    }
+    
+    func addButtons(){
         // UIボタンを作成.
         myButtonStart = UIButton(frame: CGRect(x: 0, y: 0, width: 120, height: 50))
         myButtonStop = UIButton(frame: CGRect(x: 0, y: 0, width: 120, height: 50))
@@ -94,8 +93,9 @@ class ViewController: UIViewController, AVCaptureFileOutputRecordingDelegate {
         let paths = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)
         let documentsDirectory = paths[0]
         let filePath = "\(documentsDirectory)/test.mp4"
-        let fileURL = URL(fileURLWithPath: filePath)
-        myVideoOutput.startRecording(toOutputFileURL: fileURL, recordingDelegate: self)
+        let saveURL = URL(fileURLWithPath: filePath)
+        myVideoOutput.startRecording(toOutputFileURL: saveURL,
+                                     recordingDelegate: self)
     }
     
     func onClickStopRecording(){
@@ -110,6 +110,7 @@ class ViewController: UIViewController, AVCaptureFileOutputRecordingDelegate {
     
     
     // レコード開始時
+    // TODO 使われるようにする
     private func capture(_ captureOutput: AVCaptureFileOutput!,
                          didStartRecordingToOutputFileAt fileURL: URL!,
                          fromConnections connections: [AnyObject]!) {
@@ -122,6 +123,19 @@ class ViewController: UIViewController, AVCaptureFileOutputRecordingDelegate {
                  fromConnections connections: [Any]!,
                  error: Error!) {
         print("レコード終了時")
+
+        PHPhotoLibrary.shared().performChanges({
+            PHAssetChangeRequest.creationRequestForAssetFromVideo(atFileURL: outputFileURL)
+        }) { saved, error in
+            if saved {
+                let alertController = UIAlertController(title: "Your video was successfully saved", message: nil, preferredStyle: .alert)
+                let defaultAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+                alertController.addAction(defaultAction)
+                self.present(alertController, animated: true, completion: nil)
+            }else{
+                print(error)
+            }
+        }
     }
 }
 
